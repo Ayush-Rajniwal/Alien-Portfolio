269ae13d120abdb71fcb2217f2fda4ed50f61342
Ayush Rajniwal <ayush.rajniwal@gmail.com>
2022-12-02 12:08:25 +0530

    updating details

--- experience.js
+++ experience.js
@@ -1,48 +1,91 @@
 const experience = [
-    {
-        id: 0,
-        date: "Oct-20 / Present",
-        compName: "Josh Technology Group",
-        pos: "Frontend Developer",
-        ts: ["AngularJS", "React", "SASS", "Storybook"],
-        resp: [`Working as a Frontend Engineer for a US based client BWell.`,
-            `Worked on improving legacy code and adding new features to the app.`,
-            `Transferring AngularJS code to React`,
-        ],
-        link: {
-            live: "https://play.google.com/store/apps/details?id=com.icanbwell.members&hl=en_IN&gl=US"
-        }
-    },
-    
-    {
-    id: 1,
-    date: "Apr-20 / May-20",
-    compName: "Decodr Technologies ",
-    pos: "Intern -  Web Developer",
-    ts: ["Laravel", "PHP", "CSS"],
-    resp: [`Added Email verification to the website using the Laravel framework.`,
-        `Troubleshoot and debug the errors that were crashing the website.`,
-        `Collaborated with the members from other teams to fix the chat module.`,
-        `Improved user interfaces by adding CSS effects.`
-    ],
-    link: {
-        live: "https://nexelworld.com/"
-    }
-},
-{
-    id: 2,
-    date: "Nov-19 / Jan-20",
+	{
+		id: 0,
+		date: "Mar-22 / NOW",
+		compName: "Josh Technology Group",
+		pos: "Sr. Frontend Developer",
+		ts: ["Angular", "TS", "React", "NEXT JS", "SASS", "Storybook"],
+		resp: [
+			`Reduced the build time of Microfrontends by 90%. This helped the organization in reducing the cost of GitHub Actions and also improved the developer experience.`,
+			`Implemented the multitenant app using NEXT13 that generates SSR pages. Planned the architecture that can scale for any number of clients.`,
+		],
+		link: {
+			live: null,
+		},
+	},
+	{
+		id: 1,
+		date: "Aug-21 / Mar-22",
+		compName: "Josh Technology Group",
+		pos: "Frontend Developer",
+		ts: [
+			"AngularJS",
+			"React",
+			"SASS",
+			"Storybook",
+			"MUI",
+			"TS",
+			"Webpack",
+			"ESBuild",
+		],
+		resp: [
+			`Worked on a healthcare app (BWell). Involved in day-to-day tasks of gathering requirements from the off-shore team, feature releases, building new flows, and resolving sentries.`,
+			`Maintained and contributed components to the shared UI Library built using Material UI & Typescript.`,
+			`Wrote Storybook & Unit Test Cases for the UI components with 100% coverage using React Testing Library.`,
+			`Introduced code splitting and lazy-loading via Webpack to reduce 400KB of Javascript from the initial bundle.`,
+			`Ported codebase from AngularJS to React and broke down features into Microfrontends.`,
+		],
+		link: {
+			live: null,
+		},
+	},
+	{
+		id: 2,
+		date: "Oct-20 / Aug-21",
+		compName: "Josh Technology Group",
+		pos: "Intern - Frontend Developer",
+		ts: ["AngularJS", "React", "SASS", "Storybook"],
+		resp: [
+			`Gain experience in writing production-level code on technologies like React, AngularJS, SASS Material UI, and webpack.`,
+			`Updated a few existing flows like insurance & medical search flow for one of our clients.`,
+			`Helped the team in resolving some high-priority sentries.`,
+		],
+		link: {
+			live: "https://play.google.com/store/apps/details?id=com.icanbwell.members&hl=en_IN&gl=US",
+		},
+	},
+	{
+		id: 3,
+		date: "Apr-20 / May-20",
+		compName: "Decodr Technologies ",
+		pos: "Intern -  Web Developer",
+		ts: ["Laravel", "PHP", "CSS"],
+		resp: [
+			`Added Email verification to the website using the Laravel framework.`,
+			`Troubleshoot and debug the errors that were crashing the website.`,
+			`Collaborated with the members from other teams to fix the chat module.`,
+			`Improved user interfaces by adding CSS effects.`,
+		],
+		link: {
+			live: "https://nexelworld.com/",
+		},
+	},
+	{
+		id: 4,
+		date: "Nov-19 / Jan-20",
 
-    compName: "The Villg",
-    pos: "Intern -  Full Stack DEveloper",
-    ts: ["Nodejs", "Strapi", "Bootstrap 4", "EJS"],
-    resp: [`Understand the Software Requirements and 
-    performed R&D to find the right technology stack for the project that can decrease the development time.`
-        , `Single-handed created an eCommerce
-         platform by using a headless Content Management System - StrapiJS to design the platform and APIs.`
-        , `Connect the APIs with JavaScript template (EJS) using server-side rendering.`],
-    link: {
-        live: "https://thevillg.com"
-    }
-}
-]
+		compName: "The Villg",
+		pos: "Intern -  Full Stack DEveloper",
+		ts: ["Nodejs", "Strapi", "Bootstrap 4", "EJS"],
+		resp: [
+			`Understand the Software Requirements and 
+    performed R&D to find the right technology stack for the project that can decrease the development time.`,
+			`Single-handed created an eCommerce
+         platform by using a headless Content Management System - StrapiJS to design the platform and APIs.`,
+			`Connect the APIs with JavaScript template (EJS) using server-side rendering.`,
+		],
+		link: {
+			live: "https://thevillg.com",
+		},
+	},
+];
--- projects.js
+++ projects.js
@@ -1,67 +1,79 @@
 const projects = [
-    {
-        id: 0,
-        title: "MoneyLit",
-        img: "./images/moneylit.jpg",
-        tags: ["Ionic", "React", "Capacitor"],
-        desc: "An app to track your money",
-        links: {
-            github: null,
-            live: "https://play.google.com/store/apps/details?id=com.ayushRajniwal.MoneyLit"
-        }
-    },
-    {
-    id: 1,
-    title: "COVID Live",
-    img: "./images/covid-live.png",
-    tags: ["NodeJS", "Console", "Glitch"],
-    desc: "Get COVID results in your terminal💻",
-    links: {
-        github: "https://github.com/Ayush-Rajniwal/covid.live",
-        live: "https://covid-live.glitch.me/"
-    }
-},
-{
-    id: 2,
-    title: "Weatheon",
-    img: "./images/weather.png",
-    tags: ["React Native", "OpenWeatherMap API"],
-    desc: "React Native Weather 🌦 App",
-    links: {
-        github: "github.com/Ayush-Rajniwal/Weatheon-ReactNative-WeatherApp",
-        live: "https://github.com/Ayush-Rajniwal/Weatheon-ReactNative-WeatherApp/releases/download/1.0.0/Weatheon.apk"
-    }
-},
-{
-    id: 3,
-    title: "Blockchain",
-    img: "./images/blockchain.png",
-    tags: ["Solidity", "Blockchain", "BS4"],
-    desc: " Blockchain Account Management",
-    links: {
-        github: "github.com/Ayush-Rajniwal/BlockChain-Account-Management",
-        live: null
-    }
-},
-{
-    id: 4,
-    title: "Sketchy Notebook",
-    img: "./images/sketchy.png",
-    tags: ["React", "Wiredjs"],
-    desc: "React application to take notes.",
-    links: {
-        github: "https://github.com/Ayush-Rajniwal/A-Sketchy-Notebook",
-        live: null
-    }
-},
-{
-    id: 5,
-    title: "Ulol.js",
-    img: "./images/ulol.png",
-    tags: ["JavaScript"],
-    desc: "JS library to perfom CURD on lists",
-    links: {
-        github: "https://github.com/Ayush-Rajniwal/ulol.js/",
-        live: "https://codepen.io/Ayush-Rajniwal/pen/EqWOgV"
-    }
-}]
+	{
+		id: -1,
+		title: "Cap Read SMS",
+		img: "./images/cap.png",
+		tags: ["Android", "Capacitor"],
+		desc: "Capacitor Plugin to read user's SMS",
+		links: {
+			github: "https://github.com/Ayush-Rajniwal/cap-read-sms",
+			live: "https://www.npmjs.com/package/cap-read-sms",
+		},
+	},
+	{
+		id: 0,
+		title: "MoneyLit",
+		img: "./images/moneylit.jpg",
+		tags: ["Ionic", "React", "Capacitor"],
+		desc: "An app to track your money",
+		links: {
+			github: null,
+			live: "https://play.google.com/store/apps/details?id=com.ayushRajniwal.MoneyLit",
+		},
+	},
+	{
+		id: 1,
+		title: "COVID Live",
+		img: "./images/covid-live.png",
+		tags: ["NodeJS", "Console", "Glitch"],
+		desc: "Get COVID results in your terminal💻",
+		links: {
+			github: "https://github.com/Ayush-Rajniwal/covid.live",
+			live: "https://covid-live.glitch.me/",
+		},
+	},
+	{
+		id: 2,
+		title: "Weatheon",
+		img: "./images/weather.png",
+		tags: ["React Native", "OpenWeatherMap API"],
+		desc: "React Native Weather 🌦 App",
+		links: {
+			github: "github.com/Ayush-Rajniwal/Weatheon-ReactNative-WeatherApp",
+			live: "https://github.com/Ayush-Rajniwal/Weatheon-ReactNative-WeatherApp/releases/download/1.0.0/Weatheon.apk",
+		},
+	},
+	// {
+	// 	id: 3,
+	// 	title: "Blockchain",
+	// 	img: "./images/blockchain.png",
+	// 	tags: ["Solidity", "Blockchain", "BS4"],
+	// 	desc: " Blockchain Account Management",
+	// 	links: {
+	// 		github: "github.com/Ayush-Rajniwal/BlockChain-Account-Management",
+	// 		live: null,
+	// 	},
+	// },
+	{
+		id: 4,
+		title: "Sketchy Notebook",
+		img: "./images/sketchy.png",
+		tags: ["React", "Wiredjs"],
+		desc: "React application to take notes.",
+		links: {
+			github: "https://github.com/Ayush-Rajniwal/A-Sketchy-Notebook",
+			live: null,
+		},
+	},
+	{
+		id: 5,
+		title: "Ulol.js",
+		img: "./images/ulol.png",
+		tags: ["JavaScript"],
+		desc: "JS library to perfom CURD on lists",
+		links: {
+			github: "https://github.com/Ayush-Rajniwal/ulol.js/",
+			live: "https://codepen.io/Ayush-Rajniwal/pen/EqWOgV",
+		},
+	},
+];
